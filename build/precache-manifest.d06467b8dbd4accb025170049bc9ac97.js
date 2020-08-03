0a870515411897a8137854e6d3ee0f2519dc896e
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-03 06:35:04 -0400

    ran npm build

---
+++ build/precache-manifest.d06467b8dbd4accb025170049bc9ac97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f394bdc39bc798a00dfb210549578ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673b4414824f6a0f40a7",
+    "url": "/static/css/main.bcb4105b.chunk.css"
+  },
+  {
+    "revision": "0075b5217d9f1f46f7cd",
+    "url": "/static/js/2.dee1d799.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dee1d799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673b4414824f6a0f40a7",
+    "url": "/static/js/main.4bf1f0f7.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "d934d5c9e89f44609b485fcca3a77599",
+    "url": "/static/media/manipuri-tribe.d934d5c9.jpg"
+  }
+]);
